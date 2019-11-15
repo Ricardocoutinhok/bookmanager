84238595891a9fa557f7cd17c178b2296be77d1b
Ricardocoutinhok <coutinho7947@hotmail.com>
2019-11-15 19:35:35 +0000

    Realização do ex 1

--- src/main/java/BookRunner.java
+++ src/main/java/BookRunner.java
@@ -1,7 +1,25 @@
+import models.Book;
+
 public class BookRunner {
 
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        Book livro1 = new Book();
+        livro1.setTitulo("the Art Of Computer Programming");
+        livro1.incrementaversao();
+
+        Book livro2 = new Book();
+        livro2.setTitulo("Effective Java");
+        livro2.incrementaversao();
+
+        Book livro3 = new Book();
+        livro3.setTitulo("Não sei o titulo");
+        livro3.incrementaversao();
+        livro3.incrementaversao();
+
+        System.out.println("O nome do livro é " + livro1.getTitulo() + " versao: " + livro1.getVersao());
+        System.out.println("O nome do livro é " + livro2.getTitulo() + " versao: " + livro2.getVersao());
+        System.out.println("O nome do livro é " + livro3.getTitulo() + " versao: " + livro3.getVersao());
+
     }
 }
--- src/main/java/models/Book.java
+++ src/main/java/models/Book.java
@@ -1,5 +1,28 @@
 package models;
 
 public class Book {
+    private String titulo;
+    private int versao;
 
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getVersao() {
+        return versao;
+    }
+
+    public void setVersao(int versao) {
+        this.versao = versao;
+    }
+
+    public void incrementaversao() {
+        versao = versao + 1;
+    }
 }
+
+
